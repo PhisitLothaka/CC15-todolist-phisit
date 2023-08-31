9eae36d8c77f64208f9dc46c6df07499ffbdb103
PhisitLothaka <tenphisit1998@gmail.com>
2023-08-31 16:04:50 +0700

    change Todoform => pavit

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,20 +1,28 @@
-import styles from "./TodoForm.module.scss";
-import { Button } from "../Common/Button/Button";
 import { useState } from "react";
-
-function TodoForm(props) {
-  /*
+import { Button } from "../Common/Button/Button";
+import styles from "./TodoForm.module.scss";
+/*
   props = {
-    textSubmit : strig
-    setIsOpenForm : FN
+    textSubmit : string
   }
-
-  cc1 - Form Hamdle
-  - ใช้
-
-
-
 */
+/*
+CC1- Form Handle
+- ใช้ FN ไปผูกกับ Event ชื่อ onSubmit
+- FN จะถูก Browserเรียกใช้ (เมื่อไหร่ ?) โดยส่ง parameter มา 1 ตัว (event Object)
+- โดย default ทุกปุ่มใน <form> จะทำหน้าที่ submit
+- วิธีแก้ ต้องกำหนด type ของปุ่ม
+  - type="submit" :  <button type='button'>1</button>
+  - type="button" :  <button type='submit'>2</button>
+*/
+
+/* 
+props = {
+  textSubmit : string
+  setIsOpenForm : FN
+}
+*/
+function TodoForm(props) {
   const [isError, setIsError] = useState(false);
   const [taskInput, setTaskInput] = useState("");
 
@@ -24,8 +32,12 @@ function TodoForm(props) {
   };
 
   const handleSubmit = function (event) {
+    // 1. PreventDefault
     event.preventDefault();
-    // FormValidation
+
+    // 2.ต้องรู้ก่อนว่า User พิมพ์อะไร (อยู่ใน state : taskInput)
+
+    // 3.FormValidation
     // case1 : submit ได้ => ไม่ error
     // case2 : submit ไม่ได้ => แสดง Error
     if (taskInput.trim() === "") {
@@ -33,23 +45,23 @@ function TodoForm(props) {
       setIsError(true);
       return;
     }
+    console.log("submit");
   };
 
   const handleCancel = function () {
     console.log("cancel");
-    props.setIsOpenFor(false);
+    // correctName : setIsOpenForm(false)
+    // inCorrectName : undefined(false) => บู้มเป็นโกโก้ครั้นซ์
+    props.setIsOpenForm(false);
   };
-
   return (
-    <form className={styles.todo__form__container} onSubmit={handleSubmit}>
-      {/*	Body */}
+    <form onSubmit={handleSubmit} className={styles.todo__form__container}>
       <input
         className={styles.todo__form__input}
         placeholder="Task Name"
         value={taskInput}
         onChange={handleChangeInput}
       />
-
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
         {isError ? (
