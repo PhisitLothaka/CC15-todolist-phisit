4d2f113fd5e704f50d2a3a18f7ef0460d9f4df6c
PhisitLothaka <tenphisit1998@gmail.com>
2023-08-31 14:52:34 +0700

    check error to input

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -18,16 +18,21 @@ function TodoForm(props) {
   const [isError, setIsError] = useState(false);
   const [taskInput, setTaskInput] = useState("");
 
-  const handleChange = function (event) {
+  const handleChangeInput = function (event) {
+    if (isError) setIsError(false);
     setTaskInput(event.target.value);
   };
 
   const handleSubmit = function (event) {
     event.preventDefault();
     // FormValidation
-    // case1 : submit ได้
+    // case1 : submit ได้ => ไม่ error
     // case2 : submit ไม่ได้ => แสดง Error
-    console.log("submit");
+    if (taskInput.trim() === "") {
+      console.log("Error");
+      setIsError(true);
+      return;
+    }
   };
 
   const handleCancel = function () {
@@ -42,12 +47,14 @@ function TodoForm(props) {
         className={styles.todo__form__input}
         placeholder="Task Name"
         value={taskInput}
-        onChange={handleChange}
+        onChange={handleChangeInput}
       />
 
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}></p>
+        {isError ? (
+          <p className={styles.todo__error}>Title is required</p>
+        ) : null}
         <div className={styles.todo__form__buttons}>
           <Button
             text="Cancel"
